6dab471c0d04967ed1e0f4170096da11227cdf9d
Alex-Caesar <AlexCaesar@users.noreply.github.com>
2019-10-22 14:11:27 -0500

    Autogenerated methods in DateTimeNow

---
+++ DateTimeOne.java
@@ -0,0 +1,56 @@
+import java.time.LocalDate;
+import java.time.*;
+public class DateTimeOne extends MesoDateTimeOneAbstract
+{
+	
+	private LocalDate dateNow;
+	private LocalTime timeNow;
+	public DateTimeOne(){
+		dateNow= LocalDate.now();
+		timeNow= LocalTime.now();
+		
+	}
+	
+	
+	
+	@Override
+	int getValueOfSecond() {
+		// TODO Auto-generated method stub
+		int now=0;
+		
+		
+		
+		
+		return 0;
+	}
+
+	@Override
+	void dateTimeNow() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	void sleepForFiveSec() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void dateTimeOfOtherCity() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void dateTimeDifferentZone() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void timeZoneHashMap() {
+		// TODO Auto-generated method stub
+		
+	}
+   
+}
