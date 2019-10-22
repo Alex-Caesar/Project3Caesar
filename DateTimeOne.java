91694f7c827ec8e5cdf197e89d0598c2101a74fc
Alex-Caesar <AlexCaesar@users.noreply.github.com>
2019-10-22 15:54:05 -0500

    Created DateTimeTwo:

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,37 +1,51 @@
 import java.time.LocalDate;
+import java.util.concurrent.TimeUnit;
 import java.time.*;
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
-	
+	//objects to use in class
 	private LocalDate dateNow;
 	private LocalTime timeNow;
+	
 	public DateTimeOne(){
+		//initialized variables
 		dateNow= LocalDate.now();
 		timeNow= LocalTime.now();
 		
 	}
 	
-	
-	
 	@Override
 	int getValueOfSecond() {
-		// TODO Auto-generated method stub
-		int now=0;
+		int now=timeNow.getSecond();
+		String out="The value of Second now: "+now;
 		
-		
-		
-		
-		return 0;
+		System.out.println(out);
+		return now;
 	}
 
 	@Override
 	void dateTimeNow() {
-		// TODO Auto-generated method stub
+		//taking string of date to change
+		String date=dateNow.toString();
+		String dateAdd=dateNow.toString();
+		//substringing and changing for format
+		date=date.substring(0, 4);
+		dateAdd=dateAdd.substring(5, 8);
+		
+		String out="Current Date/Time: "+date+dateAdd;
+		
+		System.out.println(out);
+		
 	}
 
 	@Override
 	void sleepForFiveSec() {
-		// TODO Auto-generated method stub
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
---
+++ sand4PR3.java
@@ -0,0 +1,11 @@
+
+public class sand4PR3 {
+
+	public static void main(String[] args) {
+		DateTimeOne test=new DateTimeOne();
+		int testInt= test.getValueOfSecond();
+		System.out.println(testInt);
+
+	}
+
+}
